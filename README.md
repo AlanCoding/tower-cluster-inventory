098967f37c454ef2ba60af4bf472329e12a204c1
AlanCoding <arominge@redhat.com>
2019-08-28 08:36:43 -0400

    add exception handling

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# tower-cluster-inventory
+### tower-cluster-inventory
 An Ansible inventory plugin to return the inventory of a live Tower cluster
 
 Use `ansible-inventory` to view the contents that the inventory produces.
@@ -6,6 +6,8 @@ Use `ansible-inventory` to view the contents that the inventory produces.
 ```
 ansible-inventory -i tower_cluster.yml --list --export --playbook-dir=.
 ```
+
+#### View Plugin Documentation
 
 Use `ansible-doc` to view the documentation.
 The search path must be set for this to work. That's due to some very technical
@@ -16,9 +18,26 @@ directory available.
 ANSIBLE_INVENTORY_PLUGINS=inventory_plugins ansible-doc -t inventory tower_cluster
 ```
 
+#### Run Playbook Against Inventory
+
 Run a playbook with this inventory.
 
 ```
 ansible-playbook -i tower_cluster.yml playbook.yml
+```
+
+#### Inventory Parsing Failures
+
+This is what happens when you run a playbook and all the parsers failed
+to parse the inventory.
+
+```
+ansible-playbook -i bad.tower_cluster.yml playbook.yml
+```
+
+Make it a failure:
+
+```
+ANSIBLE_INVENTORY_ANY_UNPARSED_IS_FAILED=True ansible-playbook -i bad.tower_cluster.yml playbook.yml
 ```
 
--- inventory_plugins/tower_cluster.py
+++ inventory_plugins/tower_cluster.py
@@ -16,6 +16,8 @@ DOCUMENTATION = r'''
             description: The network address of your Ansible Tower host.
             type: string
             required: True
+    requirements:
+        - python >= 2.7
 '''
 
 EXAMPLES = r'''
@@ -24,41 +26,50 @@ plugin: tower_cluster
 host: http://localhost:8013/
 '''
 
-from ansible.errors import AnsibleError, AnsibleParserError
-
+# Ansible internal request utilities
 from ansible.module_utils.six.moves.urllib.parse import urljoin
+from ansible.module_utils.urls import Request, ConnectionError, urllib_error
 
+from ansible.errors import AnsibleParserError
 from ansible.plugins.inventory import BaseInventoryPlugin
 
-import requests
+
+import json
 
 
 class InventoryModule(BaseInventoryPlugin):
 
     NAME = 'tower_cluster'
 
-    def verify_file(self, path):
+    def verify_file(self, path):  # Plugin interface (1)
         super(InventoryModule, self).verify_file(path)
         return path.endswith(('tower_cluster.yml', 'awx.yml'))
 
-    def parse(self, inventory, loader, path, cache=True):
+    def parse(self, inventory, loader, path, cache=True):  # Plugin interface (2)
         super(InventoryModule, self).parse(inventory, loader, path)
         self._read_config_data(path)
         host = self.get_option('host')
         ping_url = urljoin(host, 'api/v2/ping')
-        r = requests.get(ping_url)
-        data = r.json()
+
+        try:
+            response = Request().get(ping_url)
+            text = response.read()
+            data = json.loads(text)
+        except (ConnectionError, urllib_error.URLError) as e:
+            raise AnsibleParserError("Unable to connect Tower or AWX server %s: %s" % (host, e))
+        except (ValueError, TypeError) as e:
+            raise AnsibleParserError('Failed to parse json data from host, error: %s, data: %s' % (e, text))
 
         for instance_data in data['instances']:
-            self.inventory.add_host(instance_data['node'])
+            self.inventory.add_host(instance_data['node'])  # Inventory interface (1)
             self.inventory.set_variable(
                 instance_data['node'], 'capacity', instance_data['capacity']
-            )
+            )  # inventory interface (2)
 
         for group_data in data['instance_groups']:
-            group_name = self.inventory.add_group(group_data['name'])
+            group_name = self.inventory.add_group(group_data['name'])  # Inventory interface (3)
             self.inventory.set_variable(
                 group_name, 'group_capacity', group_data['capacity']
-            )
+            )  # Inventory interface (2)
             for instance in group_data['instances']:
-                self.inventory.add_child(group_name, instance)
+                self.inventory.add_child(group_name, instance)  # Inventory interface (4)
